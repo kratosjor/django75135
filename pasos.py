75ddd987d280940052a1d691b57c420b12354f37
Jordan Espinoza <jordan.espinoza.c@gmail.com>
2025-01-28 20:59:27 -0300

    Se genero el requirement

---
+++ pasos.py
@@ -0,0 +1,4 @@
+#para crear el entorno este es el codigo  python -m venv .venv
+#comando para activar entorno .venv/Scripts/activate
+#para un detalle de los paquetes instalados pip freeze > requirements.txt
+#para instalar lo del requiment el comando es pip install -r requirements.txt
